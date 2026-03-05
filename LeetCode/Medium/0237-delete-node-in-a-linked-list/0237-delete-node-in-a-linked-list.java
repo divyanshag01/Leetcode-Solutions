2944353b076a6f008853b9e39beb99220e49712e
Divyansh Agrawal <divyanshgq@gmail.com>
2026-03-05 13:44:53 +0530

    Solution added for 0237-delete-node-in-a-linked-list

--- LeetCode/Medium/0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ LeetCode/Medium/0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,19 +8,7 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        ListNode temp = node;
-        int last = 0;
-        while(temp!=null){
-            last = temp.val;
-            temp = temp.next;
-        }
-        temp = node;
-        while(temp!=null){
-            temp.val = temp.next.val;
-            if(temp.next.val==last){
-                temp.next = null;
-            }
-            temp = temp.next;
-        }
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
