b3d506bd07790bf643a63e15f56dd0c8f9370346
Divyansh Agrawal <divyanshgq@gmail.com>
2026-03-05 23:51:05 +0530

    Solution added for 0142-linked-list-cycle-ii

--- LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,16 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int flag = 0;
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) break;
+            if(slow==fast){
+                flag = 1;
+                break;
+            }
         }
-        if (fast == null || fast.next == null) return null;
-        while (head != slow) {
-            head = head.next;
+        if(flag==0) return null;
+        slow = head;
+        while(slow!=fast){
             slow = slow.next;
+            fast = fast.next;
         }
-        return head;
+        // slow = head;
+        // int cnt = 0;
+        // while(slow!=fast){
+        //     slow=slow.next;
+        //     cnt++;
+        // }
+        return slow;
     }
 }
