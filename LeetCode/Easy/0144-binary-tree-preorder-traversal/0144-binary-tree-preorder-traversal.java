751e4102d5e3a56dfb9c249180c73c260bc57151
Divyansh Agrawal <divyanshgq@gmail.com>
2026-03-12 23:57:35 +0530

    Solution added for 0144-binary-tree-preorder-traversal

--- LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,17 +15,16 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        preorder(root, ans);
-        return ans;
-
+        List<Integer> list = new ArrayList<>();
+        recurseTree(root,list);
+        return list;
     }
-    private void preorder(TreeNode root, List<Integer> ans){
-        if(root != null){ 
-            ans.add(root.val);       
-            preorder(root.left, ans);
-            preorder(root.right, ans);
+    public void recurseTree(TreeNode root,List<Integer> list){
+        if(root==null){
+            return;
         }
+        list.add(root.val);
+        recurseTree(root.left,list);
+        recurseTree(root.right,list);
     }
 }
