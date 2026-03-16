b5218ad5a094920dc55a0d368d3ecd93483bb246
Divyansh Agrawal <divyanshgq@gmail.com>
2026-03-16 23:26:32 +0530

    Solution added for 0070-climbing-stairs

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return find(n,dp);
+
+    }
+    public int find(int n, int dp[]){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = find(n-1,dp)+find(n-2,dp);
+    }
+}
