38695f877bdd4e62bb9ade5ca339fc70101a2212
Divyansh Agrawal <divyanshgq@gmail.com>
2026-03-19 23:51:22 +0530

    Solution added for 0111-minimum-depth-of-binary-tree

---
+++ LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        if(root.left == null) return minDepth(root.right) + 1;
+        if(root.right == null) return minDepth(root.left) + 1;
+
+        int left =  minDepth(root.left);
+        int right = minDepth(root.right);
+    
+        int minDepthAtNode = Math.min(left,right) + 1;
+        return minDepthAtNode;
+    }
+}
