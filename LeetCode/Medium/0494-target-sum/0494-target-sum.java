0d30c0dbc72e092059e391af60d94af490f25be3
Divyansh Agrawal <divyanshgq@gmail.com>
2026-03-23 11:33:03 +0530

    Solution added for 0494-target-sum

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,21 +1,30 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        counter.put(0, 1);
-
-        for (int n : nums) {
-            Map<Integer, Integer> temp = new HashMap<>();
-
-            for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-                int total = entry.getKey();
-                int count = entry.getValue();
-
-                temp.put(total + n, temp.getOrDefault(total + n, 0) + count);
-                temp.put(total - n, temp.getOrDefault(total - n, 0) + count);
-            }
-            counter = temp;
+        int sum = 0;
+        for(int num:nums){
+            sum+=num;
         }
+        if((target+sum)%2!=0 || target+sum<0)return 0;
 
-        return counter.getOrDefault(target, 0);        
+        target = (target+sum)/2;
+        int dp[][] = new int[nums.length][target+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return solve(nums.length-1,nums,target,dp);
+    }
+    public int solve(int idx,int nums[],int target,int dp[][]){
+        if(idx==0){
+            if(target==0 && nums[idx]==0) return 2;
+            if(target==0 || nums[idx]==target) return 1;
+            return 0;
+        }
+        if(dp[idx][target]!=-1)return dp[idx][target];
+        int notPick = solve(idx-1,nums,target,dp);
+        int pick = 0;
+        if(nums[idx]<=target){
+            pick =solve(idx-1,nums,target-nums[idx],dp);
+        }
+        return dp[idx][target]=pick+notPick;
     }
 }
