90cca552e9606454499dc59d22f29b755dfd81a3
Divyansh Agrawal <divyanshgq@gmail.com>
2026-04-18 22:21:10 +0530

    Solution added for 0787-cheapest-flights-within-k-stops

---
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,56 @@
+class Pair{
+    int wt;
+    int node;
+    public Pair(int wt,int node){
+        this.wt = wt;
+        this.node = node;
+    }
+}
+class Tuple{
+    int stop;
+    int node;
+    int wt;
+    public Tuple(int wt,int stop,int node){
+        this.stop = stop;
+        this.node = node;
+        this.wt = wt;
+    }
+}
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int num[]:flights){
+            int u = num[0];
+            int v = num[1];
+            int cost = num[2];
+            adj.get(u).add(new Pair(cost,v));
+        }
+        int distance[] = new int[n];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        Queue<Tuple> q = new LinkedList<>();
+        q.offer(new Tuple(0,0,src));
+        distance[src] = 0;
+        while(!q.isEmpty()){
+            Tuple tuple = q.poll();
+            int wt = tuple.wt;
+            int stop = tuple.stop;
+            int node = tuple.node;
+            // visited[node] = true;
+
+            if(stop>k){
+                continue;
+            }
+            for(Pair num:adj.get(node)){
+                if(distance[num.node]>wt+num.wt){
+                    distance[num.node]=wt+num.wt;
+                    q.offer(new Tuple(wt+num.wt,stop+1,num.node));
+                }
+            }
+        }
+        return distance[dst]==Integer.MAX_VALUE?-1:distance[dst];
+
+    }
+}
