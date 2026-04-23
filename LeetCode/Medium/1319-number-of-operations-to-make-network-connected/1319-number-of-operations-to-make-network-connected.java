3b747ca4506217813c3db1a88e1e6765cc02b25d
Divyansh Agrawal <divyanshgq@gmail.com>
2026-04-23 15:03:47 +0530

    Solution added for 1319-number-of-operations-to-make-network-connected

--- LeetCode/Medium/1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ LeetCode/Medium/1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,33 +1,52 @@
+class DisjointSet{
+    ArrayList<Integer> parent = new ArrayList<>();
+    ArrayList<Integer> size = new ArrayList<>();
+    public DisjointSet(int n){
+        for(int i = 0;i<n;i++){
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    public int findUPar(int u){
+        if(u==parent.get(u)){
+            return u;
+        }
+        int ulp = findUPar(parent.get(u));
+        parent.set(u,ulp);
+        return parent.get(u);
+    }
+    public int unionBySize(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v)return 1;
+        if(size.get(ulp_u)>size.get(ulp_v)){
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+            return 0;
+        }else{
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
+            return 0;
+        }
+    }
+
+}
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        int cables = connections.length;
-        if(cables<n-1) return -1;
-        boolean vis[] = new boolean[n];
-        int cnt = 0;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        DisjointSet ds = new DisjointSet(n);
+        int redundant = 0;
+        for(int num[]:connections){
+            int u = num[0];
+            int v = num[1];
+            redundant += ds.unionBySize(u,v);
+        }
+        HashSet<Integer> set = new HashSet<>();
         for(int i = 0;i<n;i++){
-            adj.add(new ArrayList<>());
+            set.add(ds.findUPar(i));
         }
-        for(int edge[]:connections){
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        for(int i = 0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                cnt++;
-            }
-        }
-        return cnt-1;
-    }
-    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean vis[]){
-        vis[node] = true;
-        for(int num:adj.get(node)){
-            if(!vis[num]){
-                dfs(num,adj,vis);
-            }
-        }
+        int components = set.size()-1;
+        if(redundant>=components) return components;
+        return -1;
+
     }
 }
