a8224cbaa0bdc790b614f8fdceb09d9507010255
Divyansh Agrawal <divyanshgq@gmail.com>
2026-05-07 19:29:53 +0530

    Solution added for 0035-search-insert-position

---
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int ans = nums.length;
+        int l = 0;
+        int r = nums.length-1;
+        if(target<nums[0])return 0;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(target<=nums[mid]){
+                ans = mid;
+                r = mid-1;
+            }else{
+                l = mid+1;
+                // ans = l;
+            }
+        }
+        return ans;
+    }
+}
