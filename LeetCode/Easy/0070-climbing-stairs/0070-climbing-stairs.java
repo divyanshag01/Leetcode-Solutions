4c70bbac3ace9869a2a0e4d45b37fb2b37689d6b
Divyansh Agrawal <divyanshgq@gmail.com>
2026-05-20 19:31:16 +0530

    Solution added for 0070-climbing-stairs

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
+        int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return find(n,dp);
-
+        dp[1]=1;
+        dp[0]=1;
+        return generate(n,dp);
     }
-    public int find(int n, int dp[]){
-        if(n<=1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = find(n-1,dp)+find(n-2,dp);
+    public int generate(int n,int[] dp){
+        if(n==1){
+            return 1;
+        }
+        if(dp[n]!=-1)return dp[n];
+        return dp[n] = generate(n-1,dp)+generate(n-2,dp);
     }
 }
