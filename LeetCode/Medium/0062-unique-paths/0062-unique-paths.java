c550b05fff19e07acf2a644d71981283b94ea848
Divyansh Agrawal <divyanshgq@gmail.com>
2026-05-21 16:20:18 +0530

    Solution added for 0062-unique-paths

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //int visited[][] = new int[m][n]; no need of visited arr as we can not revisit same cell;
-        int dp[][] = new int[m][n];
-        for(int row[]: dp){
+        int[][] dp = new int[m][n];
+        for(int row[]:dp){
             Arrays.fill(row,-1);
         }
-        return ways(m-1,n-1,dp);
+        return generate(m,n,0,0,dp);
     }
-    public int ways(int m,int n,int dp[][]){
-        if(m==0 && n==0){
+    public int generate(int m,int n,int i,int j,int[][] dp){
+        if(i==m-1 && j ==n-1){
             return 1;
         }
-        if(m<0||n<0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        // if(visited[m][n]==1){
-        //     return 0;
-        // }
-        // visited[m][n]=1;
-        int l= ways(m-1,n,dp);
-        int r= ways(m,n-1,dp);
-        // visited[m][n]=0;
-        return dp[m][n]=l+r;
+        if(i>=m || j>=n){
+            return 0;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        int right = 0;
+        int down = 0;
+        right += generate(m,n,i,j+1,dp);
+        down += generate(m,n,i+1,j,dp);
+        return dp[i][j] = right+down;
     }
 }
