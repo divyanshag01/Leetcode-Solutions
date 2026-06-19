ffbf485a79bcbdd338639f7c59750b270929a32a
Divyansh Agrawal <divyanshgq@gmail.com>
2026-06-19 12:55:30 +0530

    Solution added for 1732-find-the-highest-altitude

---
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int alt = 0;
+        for(int i: gain){
+            alt+=i;
+            max = Math.max(alt,max);
+        }
+        return max;
+    }
+}
