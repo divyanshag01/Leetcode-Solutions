39e0753a84457d703d5aa3c1b6221911e8b93c8a
Divyansh Agrawal <divyanshgq@gmail.com>
2026-06-23 21:08:53 +0530

    Solution added for 1312-minimum-insertion-steps-to-make-a-string-palindrome

---
+++ LeetCode/Hard/1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minInsertions(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        String k = sb.reverse().toString();
+        int n = s.length();
+        int dp[][] = new int[n][n];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return n-maxLen(s,k,n-1,n-1,dp);
+    }
+    public int maxLen(String s,String k,int idx1,int idx2,int dp[][]){
+        if(idx1<0 || idx2<0) return 0;
+        if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
+        int pick = 0;
+        if(s.charAt(idx1)==k.charAt(idx2)){
+            pick = 1+maxLen(s,k,idx1-1,idx2-1,dp);
+        }
+        int notPick = Math.max(maxLen(s,k,idx1-1,idx2,dp),maxLen(s,k,idx1,idx2-1,dp));
+        return dp[idx1][idx2] = Math.max(pick,notPick);
+    }
+}
