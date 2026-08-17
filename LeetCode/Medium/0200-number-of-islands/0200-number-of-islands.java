ad452047539d7b5c0fe18b84b09fcbd3a1792503
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-17 11:32:43 +0530

    Solution added for 0200-number-of-islands

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -1,27 +1,38 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
         int cnt = 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean vis[][] = new boolean[m][n];
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    fill(grid,i,j);
                     cnt++;
-                    // vis[i][j] = true;
-                    dfs(m,n,i,j,vis,grid);
                 }
             }
         }
         return cnt;
     }
-    public void dfs(int m,int n,int row,int col,boolean vis[][],char grid[][]){
-        if(row<0||row>=m || col<0 || col>=n || vis[row][col]==true
-         || grid[row][col]!='1' ) return;
-        vis[row][col] = true;
-        dfs(m,n,row+1,col,vis,grid);
-        dfs(m,n,row-1,col,vis,grid);
-        dfs(m,n,row,col+1,vis,grid);
-        dfs(m,n,row,col-1,vis,grid);
+    public void fill(char grid[][],int row,int col){
+        int drow[] = {1,0,-1,0};
+        int dcol[] = {0,-1,0,1};
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{row,col});
+        grid[row][col]='.';
+        while(!q.isEmpty()){
+            int curr[] = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            for(int i = 0;i<4;i++){
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
+                if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length){
+                    if(grid[nr][nc]=='1'){
+                        grid[nr][nc]='.';
+                        q.offer(new int[]{nr,nc});
+                    }
+                }
+            }
+        }
     }
 }
