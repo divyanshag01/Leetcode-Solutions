b0a88d8d1fb31d6521b38eabe88a16680d85d355
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-18 14:43:42 +0530

    Solution added for 0322-coin-change

--- LeetCode/Medium/0322-coin-change/0322-coin-change.java
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.java
@@ -1,24 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length][amount+1];
+        // Arrays.sort(coins);
+        int dp[][] = new int[amount+1][coins.length];
         for(int row[]:dp){
-            Arrays.fill(row,-1);
+        Arrays.fill(row,-1);
         }
-        int coin = solve(coins.length-1,amount,coins,dp);
-        return coin==(int)1e9?-1:coin;
+        int coin = check(coins,amount,coins.length-1,dp);
+        if(coin>=Integer.MAX_VALUE-10000) return -1;
+        return coin;
     }
-    public int solve(int idx,int amount,int coins[],int dp[][]){
-        if(amount==0)return 0;
-        if(idx==0){
-            if(amount%coins[idx]==0) return amount/coins[idx];
-            else return (int)1e9;
+    public int check(int coins[],int amount,int idx,int dp[][]){
+        if(amount == 0) return 0;
+        if(idx<0) return Integer.MAX_VALUE-10000;
+        if(!(amount>Integer.MAX_VALUE-10001) && dp[amount][idx]!=-1) return dp[amount][idx];
+        int pick = Integer.MAX_VALUE-10000;
+        int notPick = Integer.MAX_VALUE-10000;
+        if(amount>=coins[idx]){
+            // int num = amount/coins[idx];
+            pick=1+check(coins,amount-coins[idx],idx,dp);
         }
-        if(dp[idx][amount]!=-1)return dp[idx][amount];
-        int notPick=solve(idx-1,amount,coins,dp);
-        int pick = Integer.MAX_VALUE;
-        if(coins[idx]<=amount){
-            pick = 1+ solve(idx,amount-coins[idx],coins,dp);
-        }
-        return dp[idx][amount]=Math.min(notPick,pick);
+        notPick=check(coins,amount,idx-1,dp);
+        return dp[amount][idx] = Math.min(pick,notPick);
     }
 }
