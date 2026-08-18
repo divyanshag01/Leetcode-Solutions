8398423645338c3285d78a6b2b9a089649f2a745
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-18 11:32:57 +0530

    Solution added for 0139-word-break

--- LeetCode/Medium/0139-word-break/0139-word-break.java
+++ LeetCode/Medium/0139-word-break/0139-word-break.java
@@ -1,17 +1,30 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-
-        for (int i = 1; i <= s.length(); i++) {
-            for (String w : wordDict) {
-                int start = i - w.length();
-                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
-                    dp[i] = true;
-                    break;
+        int dp[][] = new int[s.length()][wordDict.size()];
+        for(int row[]:dp) Arrays.fill(row,-1); 
+        return check(s,wordDict,0,dp);
+    }
+    public boolean check(String s,List<String> wordDict,int idx,int dp[][]){
+        if(idx==s.length()) return true;
+        for(int i = 0;i<wordDict.size();i++){
+            if(dp[idx][i]!=-1)return dp[idx][i]==1?true:false;
+            String word = wordDict.get(i);
+            if(word.length()>s.length()-idx)continue;
+            char c = word.charAt(0);
+            if(c!=s.charAt(idx)) continue;
+            String k = s.substring(idx,idx+word.length());
+            if(k.equals(word)){
+                if(check(s,wordDict,idx+word.length(),dp)){
+                    dp[idx][i] = 1;
+                    return true;
+                }else{
+                    dp[idx][i] = 0;
                 }
             }
         }
-        return dp[s.length()];        
+        // for(int i = 0;i<wordDict.size();i++){
+        //     dp[idx][i] = false;
+        // }
+        return false;
     }
 }
