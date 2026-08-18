92e1b11a68e13649b38d3444e9a9a994004a5ef1
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-18 10:08:28 +0530

    Solution added for 0236-lowest-common-ancestor-of-a-binary-tree

--- LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,17 +9,14 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null ||root==p||root==q){
-            return root;
-        }
+        if(root == null) return root;
+        if(root==p || root==q ) return root;
+        // if(root.left==null) return root.right;
+        // if(root.right==null) return root.left;
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
-        if(left==null){
-            return right;
-        }else if(right==null){
-            return left;
-        }else{
-            return root;
-        }
+        if(left==null) return right;
+        else if(right ==null) return left;
+        else return root;
     }
 }
