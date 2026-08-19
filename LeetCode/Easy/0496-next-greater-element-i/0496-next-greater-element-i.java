224fca70c78320c13ba8d4a69271e7e09cb83eae
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-19 12:37:32 +0530

    Solution added for 0496-next-greater-element-i

--- LeetCode/Easy/0496-next-greater-element-i/0496-next-greater-element-i.java
+++ LeetCode/Easy/0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,25 +2,24 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st = new Stack<>();
         HashMap<Integer,Integer> map = new HashMap<>();
-        int arr[] = new int[nums2.length];
-        for(int i = nums2.length-1;i>=0;i--){
-            int e = -1;
-            while(!st.isEmpty()){
-                if(st.peek()>nums2[i]){
-                    e = st.peek();
-                    break;
-                }else{
-                    st.pop();
-                }
+        int n = nums2.length;
+        int ans[] = new int[n];
+        for(int i = n-1 ;i>=0;i--){
+            int num = nums2[i];
+            while(!st.isEmpty() && st.peek()<=num){
+                st.pop();
             }
-            st.push(nums2[i]);
-            arr[i]=e;
-            map.put(nums2[i],e);
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = st.peek();
+            }
+            st.push(num);
+            map.put(num,ans[i]);
         }
         for(int i = 0;i<nums1.length;i++){
             nums1[i] = map.get(nums1[i]);
         }
         return nums1;
-         
     }
 }
