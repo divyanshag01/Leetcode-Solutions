4ae1a1cbde355b18018b424e6d5d5b5197511c98
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-19 11:28:20 +0530

    Solution added for 0198-house-robber

--- LeetCode/Medium/0198-house-robber/0198-house-robber.java
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -1,18 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
+        int dp[] = new int[nums.length];
         Arrays.fill(dp,-1);
-        return check(nums,dp,n-1);
+        return maxMoney(nums,0,dp);
     }
-    public int check(int[] nums,int[] dp,int n){
-        if(n==0){
-            return nums[0];
-        }
-        if(n<0) return 0;
-        if(dp[n]!=-1)return dp[n];
-        int pick = nums[n] + check(nums,dp,n-2);
-        int notPick = check(nums,dp,n-1);
-        return dp[n] = Math.max(pick,notPick);
+    public int maxMoney(int nums[],int idx,int dp[]){
+        if(idx>=nums.length) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int pick = nums[idx] + maxMoney(nums,idx+2,dp);
+        int notPick = maxMoney(nums,idx+1,dp);
+        return dp[idx] = Math.max(pick,notPick);
     }
 }
