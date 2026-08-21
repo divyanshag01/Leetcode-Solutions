719228286620e6d3ae35d64ed9318ba3db39b951
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-21 11:15:09 +0530

    Solution added for 0137-single-number-ii

---
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+    int ans = 0;
+    for(int i= 0;i<32;i++){
+        int cnt = 0;
+        for(int num:nums){
+            if((num & (1<<i)) != 0){
+                cnt++;
+            }
+        }
+        if(cnt%3!=0){
+            ans |= 1<<i;
+        }
+    }
+        return ans;
+}}
