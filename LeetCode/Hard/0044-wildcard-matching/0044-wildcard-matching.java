4257bf1d2b36723498bb398d5e9d66ddb7bd4858
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-22 23:52:33 +0530

    Solution added for 0044-wildcard-matching

--- LeetCode/Hard/0044-wildcard-matching/0044-wildcard-matching.java
+++ LeetCode/Hard/0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,39 +1,55 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int dp[][] = new int[s.length()][p.length()];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int n = s.length();
+        int m = p.length();
+        boolean dp[][] = new boolean[n+1][m+1];
+        for(int i = 1;i<m+1;i++){
+            if(p.charAt(i-1)=='*')dp[0][i] = true;
+            else break;
         }
-        return check(s,p,s.length()-1,p.length()-1,dp);
+        dp[0][0] = true;
+        for(int i = 1;i<n+1;i++){
+            for(int j = 1;j<m+1;j++){
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if(p.charAt(j-1)=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
-    public boolean check(String s,String p,int idx1,int idx2,int dp[][]){
-        if(idx1<0 && idx2<0) return true;
-        if(idx1>=0 && idx2<0) return false;
-        if(idx1<0 && idx2>=0){
-            for(int i = idx2;i>=0;i--){
-                if(p.charAt(i)!='*')return false;
-            }
-            return true;
-        }
-        if(dp[idx1][idx2]!=-1)return dp[idx1][idx2]==1;
-        if(s.charAt(idx1)==p.charAt(idx2) || p.charAt(idx2)=='?'){
-            if(check(s,p,idx1-1,idx2-1,dp)){
-                dp[idx1][idx2] = 1;
-            }else{
-                dp[idx1][idx2] = 0;
-            }
-            return dp[idx1][idx2]==1;
-        }
-        if(p.charAt(idx2)=='*'){
-            if(check(s,p,idx1-1,idx2,dp) || check(s,p,idx1,idx2-1,dp)){
-             dp[idx1][idx2] = 1;
-            }else{
-                dp[idx1][idx2] = 0;
-            }
-            return dp[idx1][idx2]==1;
-        }
-        dp[idx1][idx2] = 0;
-        return false;
+        // return check(s,p,s.length()-1,p.length()-1,dp);
+    // }
+    // public boolean check(String s,String p,int idx1,int idx2,int dp[][]){
+    //     if(idx1<0 && idx2<0) return true;
+    //     if(idx1>=0 && idx2<0) return false;
+    //     if(idx1<0 && idx2>=0){
+    //         for(int i = idx2;i>=0;i--){
+    //             if(p.charAt(i)!='*')return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[idx1][idx2]!=-1)return dp[idx1][idx2]==1;
+    //     if(s.charAt(idx1)==p.charAt(idx2) || p.charAt(idx2)=='?'){
+    //         if(check(s,p,idx1-1,idx2-1,dp)){
+    //             dp[idx1][idx2] = 1;
+    //         }else{
+    //             dp[idx1][idx2] = 0;
+    //         }
+    //         return dp[idx1][idx2]==1;
+    //     }
+    //     if(p.charAt(idx2)=='*'){
+    //         if(check(s,p,idx1-1,idx2,dp) || check(s,p,idx1,idx2-1,dp)){
+    //          dp[idx1][idx2] = 1;
+    //         }else{
+    //             dp[idx1][idx2] = 0;
+    //         }
+    //         return dp[idx1][idx2]==1;
+    //     }
+    //     dp[idx1][idx2] = 0;
+    //     return false;
        
-    }
+    // }
 }
