d03c785b61f5366ba22bacca6b5bd9b77c8e455d
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-24 23:33:26 +0530

    Solution added for 0309-best-time-to-buy-and-sell-stock-with-cooldown

--- LeetCode/Medium/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ LeetCode/Medium/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,22 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[prices.length+2][2];
+        // for(int row[]:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        int n = prices.length;
+        for(int i = n-1;i>=0;i--){
+            dp[i][1] = Math.max(-prices[i] + dp[i+1][0],dp[i+1][1]);
+            dp[i][0] = Math.max(prices[i] + dp[i+2][1],dp[i+1][0]);
+
         }
-        return find(prices,0,1,dp);
-    }
-    public int find(int prices[],int idx,int state,int dp[][]){
-        if(idx>=prices.length) return 0;
-        if(dp[idx][state]!=-1) return dp[idx][state];
-        if(state==1){
-            int buy = -prices[idx] + find(prices,idx+1,0,dp);
-            int notBuy = find(prices,idx+1,1,dp);
-            return dp[idx][state] = Math.max(buy,notBuy);
-        }else{
-            int sell = prices[idx] + find(prices,idx+2,1,dp);
-            int notSell = find(prices,idx+1,0,dp);
-            return dp[idx][state] = Math.max(sell,notSell);
-        }
+        // return find(prices,0,1,dp);
+        return dp[0][1];
+    
+    // public int find(int prices[],int idx,int state,int dp[][]){
+    //     if(idx>=prices.length) return 0;
+    //     if(dp[idx][state]!=-1) return dp[idx][state];
+    //     if(state==1){
+    //         int buy = -prices[idx] + find(prices,idx+1,0,dp);
+    //         int notBuy = find(prices,idx+1,1,dp);
+    //         return dp[idx][state] = Math.max(buy,notBuy);
+    //     }else{
+    //         int sell = prices[idx] + find(prices,idx+2,1,dp);
+    //         int notSell = find(prices,idx+1,0,dp);
+    //         return dp[idx][state] = Math.max(sell,notSell);
+    //     }
     }
 }
