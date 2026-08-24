9e7654724b3862962bfc77d3fc103d25e64692a9
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-24 23:50:43 +0530

    Solution added for 0714-best-time-to-buy-and-sell-stock-with-transaction-fee

--- LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -3,8 +3,8 @@ class Solution {
         int dp[][] = new int[prices.length+1][2];
         int n = prices.length;
         for(int i = n-1;i>=0;i--){
-            dp[i][1] = Math.max(-(prices[i]) + dp[i+1][0],dp[i+1][1]);
-            dp[i][0] = Math.max(prices[i]-fee + dp[i+1][1],dp[i+1][0]);
+            dp[i][1] = Math.max(-(prices[i])-fee + dp[i+1][0],dp[i+1][1]);
+            dp[i][0] = Math.max(prices[i] + dp[i+1][1],dp[i+1][0]);
 
         }
         return dp[0][1];
