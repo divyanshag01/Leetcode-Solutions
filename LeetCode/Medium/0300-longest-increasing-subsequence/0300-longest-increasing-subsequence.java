d8baf230316d9f42c29ffc9aa8c2b8f359e3ddc2
Divyansh Agrawal <divyanshgq@gmail.com>
2026-08-26 12:21:40 +0530

    Solution added for 0300-longest-increasing-subsequence

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,15 +1,28 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp,1);
-        int ans = 1;
-        for(int i = 0;i<n;i++){
-            for(int j = 0 ;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
-                ans = Math.max(ans,dp[i]);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        for(int i = 1;i<nums.length;i++){
+            if(list.get(list.size()-1)<nums[i]){
+                list.add(nums[i]);
+            }else{
+                int idx = lowerBound(nums[i],list);
+                list.set(idx,nums[i]);
+            }
+        }
+        return list.size();
+    }
+    public int lowerBound(int target,ArrayList<Integer> list){
+        int l = 0;
+        int r = list.size()-1;
+        int ans = r;
+        while(l<=r){
+            int mid = l+ (r-l)/2;
+            if(list.get(mid)<target){
+                l = mid+1;
+            }else{
+                ans = mid;
+                r = mid-1;
             }
         }
         return ans;
